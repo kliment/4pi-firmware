c526fcf05a1affe3f9aab8500fc67b25df3539f2
midopple <mdoppler@gmx.at>
2012-10-09 20:41:58 +0200

    use motoropts.h in stepper_control and add function prototyps

--- src/motoropts.h
+++ src/motoropts.h
@@ -1,9 +1,28 @@
-
+/*
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 
 
 
 extern unsigned char axis_current[];
 extern unsigned char axis_ustep[];
+
+void motor_enaxis(unsigned char axis, unsigned char en);
+void motor_setdir(unsigned char axis, unsigned char dir);
+void motor_step(unsigned char axis);
+void motor_unstep();
 
 
 unsigned char microstep_mode(unsigned char usteps);
