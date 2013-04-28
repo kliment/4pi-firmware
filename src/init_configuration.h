cf122eea7730175b0ed0aa98edce800d9a49f9de
Robert Tagscherer <armageddon421@googlemail.com>
2013-04-28 08:07:48 +0200

    Set Z Jerk equal to XY in config

--- src/init_configuration.h
+++ src/init_configuration.h
@@ -121,7 +121,7 @@
 #define _ACCELERATION 1000         // Axis Normal acceleration mm/s^2
 #define _RETRACT_ACCELERATION 2000 // Extruder Normal acceleration mm/s^2
 #define _MAX_XY_JERK 20.0
-#define _MAX_Z_JERK 0.4
+#define _MAX_Z_JERK 20.0
 #define _MAX_E_JERK 5.0
 #define _MAX_ACCELERATION_UNITS_PER_SQ_SECOND {5000,5000,5000,5000}    // X, Y, Z and E max acceleration in mm/s^2 for printing moves or retracts
 
